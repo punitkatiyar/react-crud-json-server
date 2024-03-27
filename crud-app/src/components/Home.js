c6529e840291a48742ebae06a3de3f0b1cf4f298
Punit Katiyar <punit.kat@gmail.com>
2024-03-27 18:14:36 +0530

    Update Home 🎪

--- crud-app/src/components/Home.js
+++ crud-app/src/components/Home.js
@@ -1,4 +1,28 @@
-function Home(){
-  return <>data</>
+```
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+function Home() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:5000/articles")
+            .then(res => {
+                console.log(res);
+                setPosts(res.data);
+            })
+    }, [])
+    return (
+        <>
+            <div className='head'><Link to="/">Home</Link></div>
+            {posts.map(post =>
+                <div key={post.id} className='post'>
+                    <p>{post.msg}</p>
+                    <Link to={"update/" + post.id}>Edit</Link>
+
+                </div >)
+            }
+        </>
+    )
 }
 export default Home
+```
